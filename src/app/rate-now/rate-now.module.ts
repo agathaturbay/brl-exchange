34b7f664d078e45b6571ad5452731fad2ea2ef4a
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-10-04 10:17:15 -0300

    feat: add observable to get input value

---
+++ src/app/rate-now/rate-now.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurrencyService } from 'src/services/currency.service';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [CurrencyService],
+})
+export class RateNowModule { }
