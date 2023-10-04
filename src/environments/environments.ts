86ea55ceeaaffdb5868c9dc93cfaf16b01cde06e
Agatha Garcia <desenvolvedor02@senografia.com.br>
2023-10-04 15:20:09 -0300

    style: fixes on responsiveness

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-brl-exchange.actionlabs.com.br/api/1.0/open',
-  apiKey: 'keyRVZG0GHEV2KORLNA'
+  apiUrl: 'api',
+  apiKey: 'key'
 };
